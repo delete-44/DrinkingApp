8b7855be46feaacef7282a9978e2ec1a852c9ce1
delete-44 <delete44+tech@protonmail.com>
2026-05-12 16:55:47 +0100

    Bringing everything together - added a DeckSelectorEmptyState, added better warning messages on empty states for the game start

---
+++ components/decks/DeckSelectorEmptyState.tsx
@@ -0,0 +1,53 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import globalStyles from "@/assets/global-styles";
+import { plus } from "@/assets/icons/plus";
+import {
+  CONTENT_BACKDROP,
+  FONT_SIZE_MD,
+  SPACING_MD,
+  SPACING_SM,
+} from "@/src/constants/style-constants";
+import { router } from "expo-router";
+import Logo from "../Logo";
+import SVG from "../SVG";
+
+export default function DeckSelectorEmptyState() {
+  return (
+    <>
+      <View style={styles.deckSelectorWrapper}>
+        <Logo />
+
+        <Text style={globalStyles.textMd}>Add a Deck to get started:</Text>
+
+        <Pressable
+          role="button"
+          style={globalStyles.buttonHighlight}
+          onPress={() => {
+            router.navigate("/decks/new");
+          }}
+        >
+          <SVG icon={plus} width={24} height={24} />
+          <Text style={[globalStyles.buttonText, { fontSize: FONT_SIZE_MD }]}>
+            New Deck
+          </Text>
+        </Pressable>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  deckSelectorWrapper: {
+    padding: SPACING_MD,
+    marginInline: "auto",
+    maxWidth: "75%",
+
+    backgroundColor: CONTENT_BACKDROP,
+    borderRadius: SPACING_SM,
+
+    flexDirection: "column",
+    alignItems: "center",
+    gap: SPACING_SM,
+  },
+});
